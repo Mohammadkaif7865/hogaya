f88ce13d9418c0c6da5a40686c0ef3acad026804
Mohammadkaif <mohammadkaif051197@gmail.com>
2022-08-21 12:16:49 +0530

    yes me

--- firstapp/src/Component/header.js
+++ firstapp/src/Component/header.js
@@ -1,9 +1,48 @@
-function Header(props) {
-  return (
-    <div style={{ backgroundColor: "green", color: "white" , textAlign: "center" }}>
-      <h1>This is Header component {props.message}</h1>
-    </div>
-  );
-}
+// import React, { useState } from "react";
+// function Header(props) {
+//   let [val, setVal] = useState("");
+//   function handleChange(event) {
+//     setVal(event.target.value);
+//   }
+//   return (
+//     <div
+//       style={{ backgroundColor: "green", color: "white", textAlign: "center" }}
+//     >
+//       <h1>This is Header component {props.message}</h1>
+//       <input type="text" onChange={(e) => handleChange(e)} />
+//       <h3>{val}</h3>
+//     </div>
+//   );
+// }
 
+import React, { Component } from "react";
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      val: "",
+      message: "here will be the text come",
+    };
+  }
+  handleChange = (event) => {
+    this.setState({
+      val: event.target.value ? event.target.value : this.state.message,
+    });
+  };
+  render() {
+    return (
+      <div
+        style={{
+          backgroundColor: "green",
+          color: "white",
+          textAlign: "center",
+        }}
+      >
+        <h1>This is Header component {this.props.message}</h1>
+        <input type="text" onChange={(e) => this.handleChange(e)} />
+        <h3>{this.state.val}</h3>
+      </div>
+    );
+  }
+}
 export default Header;
