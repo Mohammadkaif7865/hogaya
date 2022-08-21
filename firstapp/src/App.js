cccf2d9c2c4353e9e409e27c507183ac10a40d74
Mohammadkaif <mohammadkaif051197@gmail.com>
2022-08-21 20:40:01 +0530

    first commit

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -10,30 +10,33 @@ class App extends Component {
     super(props);
     this.state = {
       message: "Hello my child",
-      data: '',
+      data: "dfh",
     };
   }
   // changeMessage = () => {
   //   this.setState({ message: "I am app your parent" });
   // };
+  componentDidMount() {
+    this.getData();
+  }
+  getData(){
+    fetch(url, { method: "GET" })
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({ data: data });
+      });
+     
+  }
   render() {
     return (
       <div className="App">
         <Header message={this.state.message} />
-        {/* <Display data={this.state.data}></Display> */}
+        <Display data={this.state.data}></Display>
         {/* <h1>This is app component</h1>
         <button onClick={this.changeMessage}>message changed</button> */}
         <Footer message={this.state.message} />
       </div>
     );
-  }
-  componentDidMount() {
-    fetch(url, { method: "GET" })
-      .then((res) => res.json())
-      .then((data) => {
-      this.setState({ data: data });
-      });
-      console.log(this.state.data);
   }
 }
 
--- firstapp/src/display.js
+++ firstapp/src/display.js
@@ -1,13 +1,14 @@
 import "./App.css";
 export default function Display(props) {
-console.log(props);
+
   return (
     <div className="App">
       {props.data.map((item, i) => 
-        <div className="imgCard">
+        <div className="imgCard" key={i}>
           <img src={item.images.img1.link} alt="img" className="imgInCard" />
         </div>
       )}
+      <h1>This is display</h1>
     </div>
   );
 }
