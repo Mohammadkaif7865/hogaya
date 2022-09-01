59bea9daba81c7f39ff9f969f4b7dbae17422bfa
mohammad kaif <mohammadkaif051197@gmail.com>
2022-09-01 10:59:53 +0530

    yes me

--- zomato/src/Component/Home/Home.js
+++ zomato/src/Component/Home/Home.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Search from "./Search";
 import QuickSearch from "./QuickSearch";
-
 const Home = (props) => {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+      /* you can also use 'auto' behaviour
+               in place of 'smooth' */
+    });
+  };
+  useEffect(() => {
+    scrollToTop();
+  }, []);
   return (
     <>
       <Search />
