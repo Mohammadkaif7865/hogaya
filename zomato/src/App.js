098d4a446ea53a4c9d8629a1e9c13d014cb09e84
mohammad kaif <mohammadkaif051197@gmail.com>
2022-09-01 11:59:57 +0530

    yes me

--- zomato/src/App.js
+++ zomato/src/App.js
@@ -5,6 +5,8 @@ import Home from "./Component/Home/Home";
 import Listing from "./Component/Listing/Listing";
 import Detail from "./Component/Details/Detail";
 import Ghost from "./Component/Ghost";
+import PlaceOrder from "./Component/Booking/placeOrder";
+import ViewOrder from "./Component/Booking/viewOrder";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
@@ -16,6 +18,8 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route path="/listing/:id" component={Listing} />
         <Route path="/details" component={Detail} />
+        <Route path="/placeOrder/:restName" component={PlaceOrder} />
+        <Route path="/viewBooking" component={ViewOrder} />
         <Route path="/*" component={Ghost} />
       </Switch>
 
