93a9a9feb5662810c58b71b9eca918a3555e81e6
mohammad kaif <mohammadkaif051197@gmail.com>
2022-09-05 12:08:09 +0530

    yes me

--- zomato/src/Component/Header.js
+++ zomato/src/Component/Header.js
@@ -1,44 +1,48 @@
 import React, { useState, useEffect } from "react";
 import { Link, withRouter } from "react-router-dom";
-import Button from "react-bootstrap/Button";
-import Offcanvas from "react-bootstrap/Offcanvas";
 import "../App.css";
- function Header(props) {
-  const [show, setShow] = useState(false);
+import "./offcanvas.css";
+function Header(props) {
+  const [show, setShow] = useState("none");
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-  function logout(){
+  const handleClose = () => setShow("none");
+  const handleShow = () => setShow("block");
+  function logout() {
     sessionStorage.clear();
     props.setName("");
     props.history.push("/");
-}
+  }
   return (
     <>
+      <div id="mySidenav" className="sidenav" style={{ display: show }}>
+        <span to="/" className="closebtn" onClick={handleClose} >&times;</span>
+        {
+          !props.name ? <Link to="/register" className="n-u-i" onClick={handleClose}>
+            <p>sign up</p>
+          </Link> : null
+        }
+        <Link to={props.name ? "/userInfo" : "/login"} className="n-u-i" onClick={handleClose}>
+          {
+            props.name ? <p>Hi {props.name}</p> : <p>log in</p>
+          }
+        </Link>
+        {
+          props.name ? <Link to="/" className="n-u-i" onClick={()=>{logout();handleClose();}}>
+            <p>LogOut</p>
+          </Link> : null
+        }
+      </div>
       <header id="header-list">
-        <Offcanvas show={show} onHide={handleClose}>
-          <Offcanvas.Header closeButton>
-            <Offcanvas.Title>Offcanvas</Offcanvas.Title>
-          </Offcanvas.Header>
-          <Offcanvas.Body>
-            Some text as placeholder. In real life you can have the elements you
-            have chosen. Like, text, images, lists, etc.
-          </Offcanvas.Body>
-        </Offcanvas>
         <div className="navbar-mine">
           <div className="left-list-nav">
-            <h1 id="my-nav-bar">
-              <Button variant="light" onClick={handleShow}>
-                <h1>
-                  <i
-                    className="fa fa-bars"
-                    aria-hidden="true"
-                    type="button"
-                    data-bs-toggle="offcanvas"
-                    data-bs-target="#demo"
-                  ></i>
-                </h1>
-              </Button>
+            <h1 id="my-nav-bar" onClick={handleShow}>
+              <i
+                className="fa fa-bars"
+                aria-hidden="true"
+                type="button"
+                data-bs-toggle="offcanvas"
+                data-bs-target="#demo"
+              ></i>
             </h1>
             <Link to="/" className="title-my-2">
               <h1
@@ -65,8 +69,8 @@ import "../App.css";
             </Link>
             {
               props.name ? <Link to="/" className="n-u-i" onClick={logout}>
-              <p>LogOut</p>
-            </Link> : null
+                <p>LogOut</p>
+              </Link> : null
             }
             <Link to="/">
               <h1
