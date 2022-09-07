de863793b302e321a1ed3c057491abb6bfa35cf8
mohammad kaif <mohammadkaif051197@gmail.com>
2022-09-07 08:26:23 +0530

    yes me

--- zomato/src/Component/login/userInfo.js
+++ zomato/src/Component/login/userInfo.js
@@ -21,18 +21,30 @@ function UserInfo(props) {
         else {
             console.log('Invalid token');
         }
-    }, [])
+    })
 
 
     return (
         <>
-            <div className="container">
-                <h3>Name : {user.name}</h3>
-                <h3>Phone Number : {user.phone}</h3>
-                <h3>Email : {user.email}</h3>
-                <Link to="/" className="btn btn-primary">Back to Home</Link>
+            {
+                user ? <div className="container">
+                    <h3>Name : {user.name}</h3>
+                    <h3>Phone Number : {user.phone}</h3>
+                    <h3>Email : {user.email}</h3>
+                    <Link to="/" className="btn btn-primary">Back to Home</Link>
 
-            </div>
+                </div> :
+                    <div className="container">
+                        <img
+                            src="https://i.ibb.co/GRf0ygr/1-Cs-J05-WEGfun-YMLGfs-T2s-XA.gif"
+                            alt="loader"
+                        />
+                        {/* https://i.ibb.co/m6TSbQ6/loader-gif.gif
+                    https://i.ibb.co/GRf0ygr/1-Cs-J05-WEGfun-YMLGfs-T2s-XA.gif"
+https://i.ibb.co/30Fq85D/loader.gif                    */}
+                        <h2>Loading....</h2>
+                    </div>
+            }
         </>
     )
 }
