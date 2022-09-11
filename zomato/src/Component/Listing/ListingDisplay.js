7c8759ab8ccd6542f9f08b1a35f71c9748ee3e97
mohammad kaif <mohammadkaif051197@gmail.com>
2022-09-11 19:07:00 +0530

    yes me

--- zomato/src/Component/Listing/ListingDisplay.js
+++ zomato/src/Component/Listing/ListingDisplay.js
@@ -9,9 +9,11 @@ const ListingDisplay = (props) => {
           return (
             <Link to={`/details?restId=${item.restaurant_id}`} key={item.restaurant_id}>
               <div className="p-i-to-2">
-                <span className="offer-mm">32% OFF</span>
-                <span className="offer-extra-mm">Pro extra 20% OFF</span>
-                <span className="dependable"><i className="bi bi-graph-up-arrow"> 12,323 delivery from here</i></span>
+                <span className="offer-mm">{item.restOffer}% OFF</span>
+                {
+                  item.isPro ? <span className="offer-extra-mm">Pro extra 20% OFF</span> : null
+                }
+                <span className="dependable"><i className="bi bi-graph-up-arrow"> {item.deliveries} delivery from here</i></span>
                 <span className="time-mm">{item.average_time} min</span>
                 <span className="float-r rate-my adjust-rate">{item.average_rating} <i className="bi bi-star-fill g-g-old"></i></span>
                 <img src={item.restaurant_thumb} alt={item.restaurant_name} />
@@ -19,7 +21,7 @@ const ListingDisplay = (props) => {
                   <h4>{item.restaurant_name}</h4>
                   <p>{item.address}</p>
                   <p>Rs. {item.cost}</p>
-                  <p>{item.mealTypes[0].mealtype_name}-{item.cuisines[0].cuisine_name}- {item.cuisines[1].cuisine_name}-{item.mealTypes[1].mealtype_name}</p>
+                  <p>{item.mealTypes[0].mealtype_name}<i className="bi bi-dot"></i>{item.cuisines[0].cuisine_name}<i className="bi bi-dot"></i> {item.cuisines[1].cuisine_name}<i className="bi bi-dot"></i>{item.mealTypes[1].mealtype_name}</p>
                 </div>
               </div>
             </Link>
